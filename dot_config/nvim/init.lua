5987a0047442f989e7cc835986ce7cdd4fe3f8fc
Piotr Banas <flunka@gmail.com>
2023-07-08 06:40:15 +0200

    set modeline

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -513,7 +513,8 @@ cmp.setup {
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+vim: ts=2 sts=2 sw=2 et
 --
 vim.wo.relativenumber = true
 vim.wo.number = true
+
